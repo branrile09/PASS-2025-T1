27ed8b0e0762de9899a206a128f54841764b275e
branrile09 <93716194+branrile09@users.noreply.github.com>
2025-02-27 14:50:37 +1030

    Add project files.

---
+++ PASS 2025 T1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace PASS_2025_T1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //leave a hello message
+            Console.WriteLine("RB waz here!!");
+            Console.WriteLine("Hello this is Angelina");
+            Console.WriteLine("I am called Brigid Ah Reum");
+
+            Console.WriteLine("hello this is willy");
+            Console.WriteLine("Hello from Queensland");
+
+
+
+;        }
+    }
+}
