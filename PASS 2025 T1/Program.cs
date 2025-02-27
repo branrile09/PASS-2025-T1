3b0a81f534786874b4fb613a83d33138c742d48a
branrile09 <93716194+branrile09@users.noreply.github.com>
2025-02-27 14:53:08 +1030

    first commit
    removed hello world

--- PASS 2025 T1/Program.cs
+++ PASS 2025 T1/Program.cs
@@ -6,7 +6,6 @@ namespace PASS_2025_T1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
             //leave a hello message
             Console.WriteLine("RB waz here!!");
             Console.WriteLine("Hello this is Angelina");
